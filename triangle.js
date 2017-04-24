ed6127b0f1f462800a75b81fd22fea8c8861c99d
Ivan Gonzalez <alu0100551266@ull.edu.es>
2017-04-24 11:14:02 +0100

    mas

---
+++ triangle.js
@@ -0,0 +1,13 @@
+"use strict"
+let Shape = require('ull-shape-ivan-ga');
+
+class Triangle extends Shape {
+    constructor(options) {
+      super(options)
+    }
+    area() {
+      return 0.5 * this.width * this.height;
+    }
+}
+Shape.Shapes.Triangle = Triangle;
+module.exports = Triangle;
