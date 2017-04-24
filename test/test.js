9468f08f9683789288b06d99a4803f21151c571a
Ivan Gonzalez <alu0100551266@ull.edu.es>
2017-04-24 19:52:34 +0100

    mas

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
-var Shape = require('@ivan-ga/ull-shape-ivan-ga');
+
 var Triangle = require('../triangle.js');
 var should = require('should');
 
 
 describe("getArea", function() {
   it("must compute the triangle area correctly. Shape object", function() {
-    let a = new Shape({ width: 100, height: 50 }, 'Triangle');
+    let a = new Triangle({ width: 100, height: 50 }, 'Triangle');
     let s = a.getArea();
     s.should.equal(2500);
   })
